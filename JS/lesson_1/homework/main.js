fc1802b0aacf7a373daba8bb3c7ee9c2f93c7f3a
ivanys23071996 <ivanys230796@gmail.com>
2021-11-11 17:51:30 +0200

    lesson_1

---
+++ JS/lesson_1/homework/main.js
@@ -0,0 +1,263 @@
+// // Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     // Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let hello = 'hello';
+// console.log(hello);
+// alert(hello);
+// document.write('hello');
+//
+// hello = ('okten');          // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(hello);
+// alert(hello);
+// document.write('.okten');
+//
+//
+//
+// let owu = 'owu';
+// console.log(owu);
+// alert(owu);
+// document.write('.owu')
+//
+// owu = 'owu1';               // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(owu);
+// alert(owu);
+// document.write('.owu1');
+//
+//
+//
+// let com = 'com';
+// console.log(com)
+// alert(com);
+// document.write('.com')
+//
+// com = '.com1';                // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(com);
+// alert(com);
+// document.write('.com1')
+//
+//
+//
+// let ua = 'ua';
+// console.log(ua);
+// alert(ua);
+// document.write('ua');
+//
+// ua = '.ua1'                  // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(ua);
+// alert(ua)
+// document.write('.ua1')
+//
+//
+//
+// let n = 1;
+// console.log(n);
+// alert(n);
+// document.write('1');
+//
+// n = 2;                      // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(n);
+// alert(n);
+// document.write('2')
+//
+//
+//
+// let u = 10;
+// console.log(u);
+// alert(u);
+// document.write('10');
+//
+// u = 11;                      // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(u);
+// alert(u);
+// document.write('11');
+//
+//
+//
+// let m = -999;
+// console.log(m);
+// alert(m);
+// document.write('-999');
+//
+// m = -1000;                   // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(m);
+// alert(m);
+// document.write('-1000');
+//
+//
+//
+// let b = 123;
+// console.log(b);
+// alert(b);
+// document.write('123');
+//
+// b = 124;                      // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(b);
+// alert(b);
+// document.write('124')
+//
+//
+//
+// let PI = 3.14;
+// console.log(PI);
+// alert(PI);
+// document.write('3.14');
+//
+// PI = 'constanta';             // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(PI);
+// alert(PI);
+// document.write('constanta');
+//
+//
+//
+// let e = 2.7;
+// console.log(e);
+// alert(e);
+// document.write('2.7');
+//
+// e = 2.8;                      // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(e);
+// alert(e);
+// document.write('2.8');
+//
+//
+//
+// let r = 16;
+// console.log(r);
+// alert(r);
+// document.write('16');
+//
+// r = 17;                      // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(r);
+// alert(r);
+// document.write('17');
+//
+//
+//
+//
+// let boolean1 = 'true';
+// console.log(boolean1);
+// alert(boolean1);
+// document.write('true');
+//
+// boolean1 = 'tru is true';   // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(boolean1);
+// alert(boolean1);
+// document.write('true is true');
+//
+//
+//
+// let boolean2 = 'false';
+// console.log(boolean2);
+// alert(boolean2);
+// document.write('false');
+//
+// boolean2 = 'false is false';  // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// console.log(boolean2);
+// alert(boolean2);
+// document.write('false is false');
+//
+
+
+// //- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+//
+// // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//
+// let firstName = 'Ivanus';
+// let middleName = 'Volodymyr';
+// let lastName = 'Mykolayovych';
+//
+// // - З'єднати їх в одну змінну person. (Не об'єкт)
+//
+// let person = {
+//     firstName: 'Ivanus',
+//     middleName: 'Volodymyr',
+//     lastName: 'Mykolayovych'
+// }
+
+
+// // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років"
+//
+//
+// let firstName = prompt('Вітаю Володимир');
+// console.log(firstName);
+//
+// let lastName = prompt('Миколайович');
+// console.log(lastName);
+//
+// let age = prompt('Тобі 18 =)');
+// console.log(age);
+
+
+// //- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+
+// // - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// //     В однакових виразах не використовувати однакові оператори!!
+// //     5 ? 6 -> true
+// // 5 ? 6 -> false
+// // 5 ? 6 -> false
+// // 5 ? 6 -> false
+// // 10 ? 10 -> true
+// // 10 ? 10 -> true
+// // 10 ? 10 -> false
+// // 10 ? 10 -> false
+// // 10 ? 10 -> false
+// // 123 ? '123' -> false
+// // 123 ? '123' -> true
+//
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 === 6);
+// console.log(5 >= 6);
+// console.log(10 === 10);
+// console.log(10 <= 10);
+// console.log(10 < 10);
+// console.log(10 > 10);
+// console.log(10 !== 10);
+// console.log(123 === '123');
+// console.log(123 !== '123');
+
+
+// // - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+// //     let str = "20";
+// // let a = 5;
+// // document.write(str + a + "<br/>");
+// // document.write(str - a + "<br/>");
+// // document.write(str * "2" + "<br/>");
+// // document.write(str / 2 + "<br/>");
+//
+//
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// console.log(typeof (str + a))
+// //type string
+// //тут а приймає тип стрінг
+// //конкатинація
+//
+// document.write(str - a + "<br/>");
+// console.log(typeof (str - a))
+// // type number
+// // немає конкатинації
+// //(20-5 = 15)
+//
+// document.write(str * "2" + "<br/>");
+// console.log(typeof (str * 2))
+// //type number
+// // немає конкатинації
+// //(20*2 = 40)
+//
+//
+// document.write(str / 2 + "<br/>");
+// console.log(typeof (str / 2))
+// //type number
+// // немає конкатинації
+// //(20/2=10)
