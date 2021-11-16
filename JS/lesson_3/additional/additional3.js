c2ba7600be4342f32edebcd65c91a5ede6fd57ae
ivanys23071996 <ivanys230796@gmail.com>
2021-11-16 21:20:35 +0200

    lesson_3(classwork)

---
+++ JS/lesson_3/additional/additional3.js
@@ -0,0 +1,31 @@
+//1. Створити пустий масив та :
+//        a. заповнити його 50 парними числами за допомоги циклу.
+//        b. заповнити його 50 непарними числами за допомоги циклу.
+//        c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+//        d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+let arr = [];
+
+// //a. заповнити його 50 парними числами за допомоги циклу.
+// for (let i = 2, j = 0; i <= 100; i += 2, j++) {
+//     arr[j] = i;
+// }
+// console.log(arr);
+
+// //b. заповнити його 50 непарними числами за допомоги циклу.
+// for (let i = 1, j = 0; i <= 100; i += 2, j++) {
+//     arr[j] = i;
+// }
+// console.log(arr)
+
+// //c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+// for (let i = 0; i < 20; i++) {
+//     arr[i] = Math.random();
+// }
+// console.log(arr)
+
+//d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+for (let i = 0; i < 20; i++) {
+    let randomFloat = (min =8, max=732) => Math.random() * (max - min) + min;
+    arr[i]=randomFloat()
+}
+console.log(arr)
