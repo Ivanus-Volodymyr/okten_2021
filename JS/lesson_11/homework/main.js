045539e0b6be4e895ad08937f35f085bb9f7ff43
ivanys23071996 <ivanys230796@gmail.com>
2021-12-10 16:51:26 +0200

    lesson_11(homework)

--- JS/lesson_11/homework/main.js
+++ JS/lesson_11/homework/main.js
@@ -21,28 +21,40 @@
 // div.style.margin = '50px';
 // document.body.append(div);
 
-// // -створити форму з інпутами для model,type та volume автівки.
-// // при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
-// let divElement = document.createElement('div');
-// let formElement = document.createElement('form');
-// let model = document.createElement('input');
-// model.type = 'text';
-// model.placeholder = 'model';
-// let type = document.createElement('input');
-// type.type = 'text';
-// type.placeholder = 'type';
-// let volume = document.createElement('input');
-// volume.type = 'number';
-// volume.placeholder = 'volume';
-// let buttonElement = document.createElement('button');
-// buttonElement.innerText = 'Submit';
-// buttonElement.addEventListener('click', function () {
-//     let mas = [{model: model.value}, {type: type.value}, {volume: volume.value}];
-//     let stringifyMas = JSON.stringify(mas);
-//     localStorage.setItem('Car', stringifyMas);
-// })
-//
-// formElement.append(model, type, volume, buttonElement)
-// divElement.appendChild(formElement);
-// divElement.style.margin = '50px 0 0 50px';
-// document.body.appendChild(divElement);
+// -створити форму з інпутами для model,type та volume автівки.
+// при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+let divElement = document.createElement('div');
+let formElement = document.createElement('form');
+let model = document.createElement('input');
+model.type = 'text';
+model.placeholder = 'model';
+let type = document.createElement('input');
+type.type = 'text';
+type.placeholder = 'type';
+let volume = document.createElement('input');
+volume.type = 'number';
+volume.placeholder = 'volume';
+let buttonElement = document.createElement('button');
+buttonElement.innerText = 'Submit';
+
+let result = [];
+let stringify = JSON.stringify(result);
+localStorage.setItem('car', stringify);
+
+buttonElement.addEventListener('click', function (e) {
+    e.preventDefault();
+    let item = localStorage.getItem('car');
+    let parse = JSON.parse(item);
+    parse.push({model: model.value, type: type.value, volume: volume.value});
+    localStorage.setItem('car', JSON.stringify(parse));
+    model.value = '';
+    type.value = '';
+    volume.value = '';
+    return result;
+});
+
+
+formElement.append(model, type, volume, buttonElement);
+divElement.appendChild(formElement);
+divElement.style.margin = '50px 0 0 50px';
+document.body.appendChild(divElement);
